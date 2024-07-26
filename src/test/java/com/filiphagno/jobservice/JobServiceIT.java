e29e3d72352d0fcfc1011c67b2ec040e1d52a5e5
Filip <fhagno@yahoo.com>
2024-07-26 08:50:08 +0200

    Add integration tests via test containers

---
+++ src/test/java/com/filiphagno/jobservice/JobServiceIT.java
@@ -0,0 +1,52 @@
+package com.filiphagno.jobservice;
+
+import com.filiphagno.jobservice.dto.JobDto;
+import com.filiphagno.jobservice.generic.BaseTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.util.Set;
+
+@SpringBootTest
+@AutoConfigureWebTestClient
+class JobServiceIT extends BaseTest {
+
+	@Autowired
+	private WebTestClient client;
+
+	@Test
+	void allJobsTest() {
+		this.client.get()
+				.uri("/job/all")
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody().jsonPath("$").isNotEmpty();
+	}
+
+	@Test
+	public void searchBySkillsTest() {
+		this.client.get()
+				.uri("/job/search?skills=java")
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody()
+				.jsonPath("$.size()").isEqualTo(2);
+	}
+
+	@Test
+	public void postJobTest() {
+		var dto = JobDto.create(null, "k8s engr", "google", Set.of("k8s"), 200000, true);
+		this.client.post()
+				.uri("/job")
+				.bodyValue(dto)
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody()
+				.jsonPath("$.id").isNotEmpty()
+				.jsonPath("$.description").isEqualTo("k8s engr");
+	}
+}
